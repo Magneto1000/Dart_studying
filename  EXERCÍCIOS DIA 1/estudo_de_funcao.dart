1947b8666e972db2c440ef0cf773fae1b5ce6723
Tito Neto <magnetotito@gmail.com>
2026-06-22 11:41:20 -0300

    Nova pasta com exercícios

---
+++  EXERCÍCIOS DIA 1/estudo_de_funcao.dart
@@ -0,0 +1,12 @@
+//Definição da função
+imprimirInteiro(int umNumero) {
+	print('O número é: $umNumero'); // imprime no console
+}
+
+// Aqui é onde o começa a execução
+main() {
+	var numero = 43; // Declara e inicializa a variável
+	imprimirInteiro(numero); // Chana a funão
+
+}
+
