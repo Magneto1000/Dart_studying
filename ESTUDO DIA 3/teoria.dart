21b2b503015251f83173f4480087292b04b92694
Magneto1000 <magnetotito@gmail.com>
2026-06-28 23:39:15 -0300

    Atualização dos dados de exercícios

--- ESTUDO DIA 3/teoria.dart
+++ ESTUDO DIA 3/teoria.dart
@@ -37,4 +37,74 @@ de volta parao fluxo principal.
 ----------------------------------------------------
 2. PARAMÊTROS OBRIGATÓRIOS vs. NOMEADOS PPCIONAIS
 ----------------------------------------------------
+O Dart possuí um dos sistemas de parametização mais flexíveis e expressivos entre as linguagens modernas.
+Ele divide os parâmetros em duas categorias.
 
+a) Paramêtros POSICIONAIS OBRIGATÓRIOS (Abordagem Tradicional)
+São aqueles definidos por padrão. Quem invoca a função é obrigado a passar os valores
+exatamente na mesma ordem em que foram declaradas.
+
+----------------------------------------------------
+Dart.exemplo
+
+// Declaração
+void conectarBanco(String url, int porta) {
+  print('Conectando e $url na porta $porta');
+}
+void main() {
+  // Execução obrigatória respeitando a ordem posicional
+  conectarBnaco('Localhost', 5432);
+}
+-----------------------------------------------------------
+
+OBSERVAÇÃO: A desvantagem é se a função tiver muitos parâmetros, fica fácil esquecer a ordem ou o que cada valor signfíca visualmente.
+
+---------------------------------------------
+B) PARÂMETROS NOMEADOS {} (A base do framework Flutter)
+
+Ao envolver os parâmetros de uma função com chaves {}, nós mudamos as regras do jogo.
+A partir desde momento:
+1º A ordem dos fatoresnão importa mais no chamado;
+2º Nós somos obrigados apassar o nome do parâmetro seguindo por dois pontos ':' ao
+chamar a função;
+3º Por padrão, parâmetros dentro de chaves tornam-se opcionais. Para torná-los
+obrigatórios mesmo dentro das chaves, usamos a palavra reservada required.
+
+Abaixo podemos ver a clareza e elegância dessa sintaxe aplicada.
+--------------------------------------------
+Dart.exemple
+
+// Declaração: 'remetente' é obrigatório, 'assunto' tem um valor padrão se não for passado
+void enviarEmail({required String remetente, String assunto = 'Sem Assunto'}) {
+  print('De: $remetente | Assunto: $assunto');
+}
+
+void main() {
+  // A ordem não importa e o código fica extremamente legível
+  enviarEmail(assunto: 'Relatório Over Power', remetente: john@upp.com);
+
+  // Como 'assunto' tem valor padrão, posso omiti-lo com segurança
+  enviarEmail(remetente: 'thayna@upp.com');
+}
+---------------------------------------------------
+
+3. FUNÇÕES DE LINHA ÚNICA: Arrow Syntax ( => )
+
+Qunado escrevemos funções simples, cujo corpo executa apenas uma única expressão de código e já retorna
+e já retorna o resultando imediatamente, o uso de chaves {} e da palavra return
+gera uma poluição visual desnecessária.
+
+O Dart simplifíca isso com a Arrow Syntax (Síntaxa de Seta =>). A seta substitui as chaves e faz o 
+return de forma implícita e automática sob os panos.
+
+---------->
+VERSÃO TRADICIONAL:
+Dart.
+int somar(int a, int b) {
+  return a + b;
+}
+--------->
+Versão com Arrow Syntax (Padrão Clean Code)
+Dart.
+int somar(int a, int b) => a + b; // O resultado da conta é retornando automaticamente
+----------->
