9170a06f26b76f7bd892d03ea33ec8b0643603b2
Tito Neto <magnetotito@gmail.com>
2026-07-09 17:52:34 -0300

    Atualizando desafio

---
+++ ESTUDO DIA 4/MARATONA ORIENTAÇÃO A OBJETOS/exercicio1.dart
@@ -0,0 +1,7 @@
+/*
+Imagine que você trabalha no Instagram. Você não desenha cada post do zero. Você cria uma
+fôrma (classe) que diz: "Todo post precisa ter o nome do usuário e a foto". Quando o usuário
+arrasta o dedo para cima, o aplicativo pega os dados do banco e joga nessa fôrma, gerando
+um post real (objeto) na tela. 
+
+*/
